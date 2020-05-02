2e56322f1f7b0bf1e97bbf6251b677bb787aac6f
Subhajit Karmakar <skarma4@ext.uber.com>
2020-05-02 22:55:44 +0530

    All changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-// import React, { Component } from 'react';
-import React, { useState } from 'react';
+import React, { Component } from 'react';
+// import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 // import Post from './Post/Post';
 
-// class app extends Component {
+
 //this is the example of react hooks by using react hooks we can manage state inside the functional component
 //limitations are :
 // 1 > In the class based components easily the state managed suppose there is two states in the
@@ -14,43 +14,44 @@ import Person from './Person/Person';
 
 
 
-const app = (props) => {
-  const [personsState, setPersonsState] = useState({
+// const app = (props) => {
+//   const [personsState, setPersonsState] = useState({
+//     persons: [
+//       { name: 'Subhajit', age: 26 },
+//       { name: 'Santu', age: 25 }
+//     ]
+//   });
+// // Or for using updating the otherState we need to follow this approach.
+//   const [otherState, setOtherState] = useState({
+//     others: [
+//       { specialmove: 'Sharingan', type: 'Friendly' },
+//       { specialmove: 'Byakugan', type: 'Rogue' }
+//     ]
+//   })
+//   console.log(personsState, otherState)
+
+//   const switchHandler = () => {
+//     //   // alert('Was clicked!!')
+//     setPersonsState({
+//       persons: [
+//         { name: 'XOXO', age: 100 },
+//         { name: 'LMAO', age: 10 }
+//       ]
+
+
+//     });
+
+//   };
+class app extends Component {
+  state = {
     persons: [
       { name: 'Subhajit', age: 26 },
       { name: 'Santu', age: 25 }
-    ]
-  });
-// Or for using updating the otherState we need to follow this approach.
-  const [otherState, setOtherState] = useState({
-    others: [
-      { specialmove: 'Sharingan', type: 'Friendly' },
-      { specialmove: 'Byakugan', type: 'Rogue' }
-    ]
-  })
-  console.log(personsState, otherState)
-
-  const switchHandler = () => {
-    //   // alert('Was clicked!!')
-    setPersonsState({
-      persons: [
-        { name: 'XOXO', age: 100 },
-        { name: 'LMAO', age: 10 }
-      ]
-
-
-    });
-
-  };
-  // state = {
-  //   persons: [
-  //     { name: 'Subhajit', age: 26 },
-  //     { name: 'Santu', age: 25 }
-  //   ],
-  //in the react hooks for this state if we click on switch Handler it will only update the person properties for that we need to
-  //update in the switchHandler that otherState :personsState.otherState in line 40
-  //   otherState: 'New State'
-  // }
+    ],
+    //in the react hooks for this state if we click on switch Handler it will only update the person properties for that we need to
+    //update in the switchHandler that otherState :personsState.otherState in line 40
+    otherState: 'New State'
+  }
   // switchHandler = () => {
   //   // alert('Was clicked!!')
   //   this.setState({
@@ -61,28 +62,67 @@ const app = (props) => {
 
 
   //   })
+  // }
+  // Dynamic representation
+  switchHandler = (newName) => {
+    // alert('Was clicked!!')
+    this.setState({
+      persons: [
+        { name: newName, age: 100 },
+        { name: 'LMAO', age: 10 }
+      ]
 
 
-  // render() {
-  return (
-    <div className="App">
-      <h1>Subhajit</h1>
-      <p>This is really cool stuff</p>
-      {/* <button onClick = {this.switchHandler}>Switch</button> */}
-      <button onClick={switchHandler}>Switch</button>
-      <Person name="Mew2" age="100000000000" />
-      {/* <Post tilte="My title" /> */}
-      <Person name={personsState.persons[0].name} age={personsState.persons[0].age} >A new day has come</Person>
-      <Person name={personsState.persons[1].name} age={personsState.persons[1].age} />
+    })
+  }
+  // this is for two way binding
+  nameHandler = (event) => {
+    // alert('Was clicked!!')
+    this.setState({
+      persons: [
+        // here event refers to extract the obj target is inputbox and value is what ever we are typing
+        //this shows us how we dynamically update something passing down event references and how we manage inputs
+        { name: event.target.value, age: 100 },
+        { name: 'LMAO', age: 10 }
+      ]
 
-      {/* <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>A new day has come</Person>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} /> */}
 
-    </div>
-  );
-  // return React.createElement('div', {className: 'App'},React.createElement('h1', null, 'My Name Is Subhajit'))
-//specialmove={otherState.others[0].specialmove} type={otherState.others[0].type}
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Subhajit</h1>
+        <p>This is really cool stuff</p>
+        {/* This is for hooks we dont have to include this as dumb component */}
+        {/* <button onClick={switchHandler}>Switch</button> */}
+        {/* <Post tilte="My title" /> */}
+        {/* refering to the click prop */}
+        {/* This is using hooks */}
+        {/* <Person name={personsState.persons[0].name} age={personsState.persons[0].age} >A new day has come</Person>
+      <Person name={personsState.persons[1].name} age={personsState.persons[1].age} /> */}
+        {/* 1st approach of getting the data */}
+        {/* <button onClick={this.switchHandler.bind(this, 'Tyson')}>Switch</button>  */}
+        Here this refers to the switch handled and arrow function is passing the object
+        <button onClick={() => this.switchHandler('Max')}>Switch</button>
+        <Person name="Mew2" age="100000000000" />
+        <Person
+          // this is we are passing down the obj
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+          // we are using click property and by this keyword we are fetching the new data from switchHandler
+          click={this.switchHandler.bind(this, 'Kai')}
+          changed={this.nameHandler}>A new day has come
+          </Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+
+      </div>
+    );
+    // return React.createElement('div', {className: 'App'},React.createElement('h1', null, 'My Name Is Subhajit'))
+    //specialmove={otherState.others[0].specialmove} type={otherState.others[0].type}
+
+  }
 
 }
-
 export default app;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,11 +1,16 @@
 import React  from 'react';
-
+import './Person.css';
 const person = (props) => {
   console.log(props)
-  return <div>
-    <p>I am {props.name} my age is {props.age} a Software Developer and i got {Math.floor(Math.random() * 10)} years Experience more over i have {props.type} in type and {props.specialmove} power</p>
-    <p>{props.children}</p>
+  return (
+    <div className="Person">
+    <p onClick={props.click}>I am {props.name} my age is {props.age} a Software Developer and i got
+    {Math.floor(Math.random() * 10)} years Experience </p>
+      {/* <p>{props.children}</p> */}
+      Here on change
+      <input type="text" onChange={props.changed} value={props.name}/>
   </div>
+  )
 }
 
 export default person;
